a8253513b03ca6c7b882eca6c5ec67b519447d5d
Sandy Gifford <sgifford2@NY731-20-143BX.local>
2015-10-30 10:57:59 -0400

    SASS Support - tied CSS back into HTML

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,6 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean: {
-      build: [
-        'public/js/app.js'
-      ]
-    },
     jshint: {
       files: ['Gruntfile.js', 'source/js/*.js'],
       options: {
@@ -22,26 +17,29 @@ module.exports = function (grunt) {
           }
         },
         files: {
-          'public/js/app.js'  : 'source/js/script.js'
+          'public/js/app.js' : 'source/js/script.js'
         }
       }
     },
-    watch: {
-      options: {
-        atBegin: true
-      },
-      dev: {
-        files: ['source/js/*.js'],
-        tasks: ['jshint', 'browserify']
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'public/css/style.css': 'source/css/style.scss'
+        }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  grunt.loadNpmTasks( 'grunt-browserify'     );
+  grunt.loadNpmTasks( 'grunt-contrib-sass'   );
 
-  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('default', [
+    'jshint', 'browserify',  // JS
+    'sass'                   // CSS
+  ]);
 
 };
